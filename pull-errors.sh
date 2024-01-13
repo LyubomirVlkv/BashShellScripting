3c67e5e3c3a12225a0b0d95dfb0518136adb51c6
Lyubomir Velikov <64361260+LyubomirVlkv@users.noreply.github.com>
2024-01-13 17:49:34 +0200

    Delete pull-errors.sh

--- pull-errors.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Author Lyubomir Velikov
-# Description Script to pull error messages from /var/log/messages
-
-
-grep -i error /home/lyubo/bashscripting/Januaryi\ Scripts/messages > /home/lyubo/bashscripting/Januaryi\ Scripts/output-error
-
