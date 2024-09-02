989055b311a5e08444a054bdae963745dd5d279d
LyubomirVlkv <lobomir4oo10@gmail.com>
2024-09-02 19:20:47 +0300

    remove some files

--- jan-scripts/admin-commands.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Author: Lyubomir Velikov
-# Description: Format the output of admin commands
-
-date | awk '{print $2}'
-uptime | awk '{print $3}'
-df -h | grep root
--- jan-scripts/pull-fail.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Author Lyubomir Velikov
-# Description Script to pull error messages from /var/log/messages
-
-
-grep -i fail /home/lyubo/bashscripting/Januaryi\ Scripts/messages
